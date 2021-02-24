e237aa2ca87c797232f65784db2d33c33980102f
hakuunabatata <emachadomartins1999@gmail.com>
2021-02-24 20:28:27 -0300

    add Countdown hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,9 @@ import styles from "../styles/pages/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
       <ExperienceBar />
 
       <section>
